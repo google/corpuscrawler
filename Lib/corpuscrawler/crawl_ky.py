eca58dd9f136823a3f689b6d1b2f3625542d5b5a
Sascha Brawer <sascha@brawer.ch>
2017-09-27 13:08:58 +0200

    [ky] Crawl another Kyrgyz-language news site

--- Lib/corpuscrawler/crawl_ky.py
+++ Lib/corpuscrawler/crawl_ky.py
@@ -15,10 +15,38 @@
 from __future__ import absolute_import, print_function, unicode_literals
 import re
 
-from corpuscrawler.util import crawl_bbc_news, crawl_udhr
+from corpuscrawler.util import cleantext, crawl_bbc_news, crawl_udhr, urlpath
 
 
 def crawl(crawler):
     out = crawler.get_output(language='ky')
     crawl_udhr(crawler, out, filename='udhr_kir.txt')
     crawl_bbc_news(crawler, out, urlprefix='/kyrgyz/')
+    crawl_azattyk_org(crawler, out)
+
+
+def crawl_azattyk_org(crawler, out):
+    sitemap = crawler.fetch_sitemap('https://www.azattyk.org/sitemap.xml')
+    for url in sorted(sitemap.keys()):
+        if not urlpath(url).startswith('/a/'):
+            continue
+        doc = crawler.fetch(url)
+        if doc.status != 200:
+            continue
+        html = doc.content.decode('utf-8')
+        pubdate = re.search(r'"dateModified":"([^"]+)"', html)
+        if pubdate is not None:
+            pubdate = cleantext(pubdate.group(1)).replace(' ', 'T')
+        title = re.search(r'<title>(.+?)</title>', html).group(1)
+        text = html.split('content-offset">', 1)[1].split('</div>')[0]
+        paras = [title] + text.replace('<br />', '\n').splitlines()
+        paras = filter(None, [cleantext(p) for p in paras])
+        paras = [p for p in paras if not p.startswith('http')]
+        if not paras:
+            continue
+        out.write('# Location: %s\n' % url)
+        out.write('# Genre: News\n')
+        if pubdate:
+            out.write('# Publication-Date: %s\n' % pubdate)
+        for p in paras:
+            out.write(p + '\n')
