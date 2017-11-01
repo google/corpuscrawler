d9997f763e0d9c6b272fedf53288f367e3fe2a46
Jim O'Regan <jaoregan@tcd.ie>
2017-11-01 19:46:15 +0000

    some fixes; also crawl the sport section

--- Lib/corpuscrawler/crawl_mi.py
+++ Lib/corpuscrawler/crawl_mi.py
@@ -28,23 +28,27 @@ def crawl(crawler):
 def _scrape_maoritelevision(crawler, out):
     articlelist = set()
     articlelist.add('http://www.maoritelevision.com/mi/purongo/purongo-hou')
-    for i in range(1, 100):
+    articlelist.add('http://www.maoritelevision.com/mi/purongo/hakinakina')
+    for i in range(1, 101):
         articlelist.add('http://www.maoritelevision.com/mi/purongo/purongo-hou?page=%d' % i)
+        articlelist.add('http://www.maoritelevision.com/mi/purongo/hakinakina?page=%d' % i)
     links = set()
+    pubdate_regex = re.compile(r'<time datetime="([0-9T:+\-]{25})"')
     for url in articlelist:
         doc = crawler.fetch(url)
         if doc.status != 200:
             continue
         content = doc.content.decode('utf-8')
         for articlepiece in content.split('<article')[1:]:
-            for link in re.findall('<a href="(/mi/purongo/[^"]*)"', articlepiece):
-                links.add('http://www.maoritelevision.com%s' % link)
-    for link in links:
+            for artlink in re.findall('<a href="(/mi/purongo/[^"]*)"', articlepiece):
+                if not artlink.startswith('/mi/purongo/purongo-hou'):
+                    links.add('http://www.maoritelevision.com%s' % artlink)
+    for url in links:
         doc = crawler.fetch(url)
         if doc.status != 200:
             continue
         html = doc.content.decode('utf-8')
-        if '<html  lang="mi"' not in html:
+        if 'lang="mi"' not in html:
             continue
         if 'itemprop="articleBody"' not in html:
             continue
@@ -53,19 +57,22 @@ def _scrape_maoritelevision(crawler, out):
         if pubdate is None: pubdate = doc.headers.get('Last-Modified')
         if pubdate is None: pubdate = sitemap[url]
         out.write('# Location: %s\n' % url)
-        out.write('# Genre: News\n')
+        if '/hakinakina/' in url:
+            out.write('# Genre: Sport\n')
+        else:
+            out.write('# Genre: News\n')
         if pubdate: out.write('# Publication-Date: %s\n' % pubdate)
         # These news stories are a parallel (or at least comparable) corpus, so keeping
         # the link to the English article
         english = re.search(r'<a href="(/news/[^"]*)" class="language-link" lang="en">', html)
         if english: english = 'http://www.maoritelevision.com%s' % english.group(1)
-        if english: out.write('English: %s\n' % english)
+        if english: out.write('# English: %s\n' % english)
         tags = set()
         tagshtml = html.split('<ul class="tags">')[1].split('</ul>')[0]
         for tag in re.findall(r'<a href="(?:[^"]*)">([^<]*)</a>', tagshtml):
             tags.add(cleantext(tag))
         if len(tags) is not 0:
-            out.write('Tags: %s' % ', '.join(tags))
+            out.write('# Tags: %s' % ', '.join(tags))
         title = re.search(r'<title>(.+?)</title>', html)
         if title: title = striptags(title.group(1).split('| Māori')[0]).strip()
         if title: out.write(cleantext(title) + '\n')
