1134764dc9290458d3d5c40b95db7e9d93495fc5
Jim O'Regan <jaoregan@tcd.ie>
2018-05-07 09:38:33 +0100

    start of US Embassy crawler

--- Lib/corpuscrawler/crawl_pl.py
+++ Lib/corpuscrawler/crawl_pl.py
@@ -13,10 +13,47 @@
 # limitations under the License.
 
 from __future__ import absolute_import, print_function, unicode_literals
-from corpuscrawler.util import crawl_deutsche_welle, crawl_udhr
-
+import re
+from corpuscrawler.util import (
+    crawl_deutsche_welle, crawl_udhr, extract, clean_paragraphs, urlpath
+)
 
 def crawl(crawler):
     out = crawler.get_output(language='pl')
     crawl_udhr(crawler, out, filename='udhr_pol.txt')
-    crawl_deutsche_welle(crawler, out, prefix='/pl/')
+    #crawl_deutsche_welle(crawler, out, prefix='/pl/')
+    crawl_pl_usembassy_gov(crawler)
+
+def crawl_pl_usembassy_gov(crawler):
+    top_sitemap = crawler.fetch_sitemap('https://pl.usembassy.gov/sitemap_index.xml')
+    pubdate_regex = re.compile(
+        r'<meta property="article:published_time" content="([^"]*)"'
+    )
+    links = set()
+    for sm_url in sorted(top_sitemap.keys()):
+        sitemap = crawler.fetch_sitemap(sm_url)
+        for key in sitemap.keys():
+            if 'pl.usembassy.gov/pl' in urlpath(key).startswith('/pl/'):
+                links.add(key)
+    for link in sorted(links):
+        result = crawler.fetch(link)
+        if result.status != 200:
+            continue
+        html = result.content.decode('utf-8')
+        title = extract('<title>', '</title>', html)
+        title = title if title else ''
+        title = title.split(' | ')[0] if ' | ' in title else title
+        pubdate_match = pubdate_regex.search(html)
+        pubdate = pubdate_match.group(1) if pubdate_match else None
+        if pubdate is None: pubdate = fetchresult.headers.get('Last-Modified')
+        if pubdate is None: pubdate = sitemap[url]
+        content = extract('<div class="entry-content">',
+            '<!-- AddThis Advanced Settings above via filter on the_content -->')
+        paras = clean_paragraphs(title + '<p/>' + content)
+        if paras:
+            out.write('# Location: %s\n' % link)
+            out.write('# Genre: Diplomatic')
+            if pubdate:
+                out.write('# Publication-Date: %s\n' % pubdate)
+            out.write('\n'.join(paras) + '\n')
+
