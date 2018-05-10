3ce125fa88acf12c9e725c3150b0cc3a83c8835c
Sascha Brawer <sascha@brawer.ch>
2018-05-10 11:00:56 +0200

    [iba] Crawl a larger corpus for the Iban language (#39)

--- Lib/corpuscrawler/crawl_iba.py
+++ Lib/corpuscrawler/crawl_iba.py
@@ -13,9 +13,46 @@
 # limitations under the License.
 
 from __future__ import absolute_import, print_function, unicode_literals
-from corpuscrawler.util import crawl_bibleis
+import re
+from corpuscrawler.util import (
+    clean_paragraphs, crawl_bibleis, extract, urljoin
+)
 
 
 def crawl(crawler):
     out = crawler.get_output(language='iba')
     crawl_bibleis(crawler, out, bible='IBATIV')
+    _crawl_utusan_borneo_berita_iban(crawler, out)
+
+
+def _crawl_utusan_borneo_berita_iban(crawler, out):
+    for url in sorted(_find_urls_utusan_borneo_berita_iban(crawler)):
+        doc = crawler.fetch_content(url)
+        title = re.search(r'<meta property="og:title" content="(.+?)"', doc)
+        title = title.group(1) if title else ''
+        paras = clean_paragraphs('<h1>%s</h1>' % title +
+                                 extract('<p>', '<footer>', doc))
+        pubdate = re.search(
+            r'<meta property="article:published_time" content="([\d\-]+)"',
+            doc)
+        pubdate = pubdate.group(1) if pubdate else None
+        if paras:
+            out.write('# Location: %s\n' % url)
+            out.write('# Genre: News\n')
+            if pubdate:
+                out.write('# Publication-Date: %s\n' % pubdate)
+            out.write('\n'.join(paras) + '\n')
+
+
+def _find_urls_utusan_borneo_berita_iban(crawler):
+    urls = set()
+    main = crawler.fetch_content('http://www.utusanborneo.com.my/iban')
+    num_pages = max(map(int, re.findall('\?page=(\d+)', main)))
+    for p in range(0, num_pages):
+        index_url = 'http://www.utusanborneo.com.my/iban'
+        if p > 0:
+            index_url = index_url + '?page=%d' % p
+        for url in re.findall(r'href="(/\d{4}/\d{2}/\d{2}/[^"]+)"',
+                              crawler.fetch_content(index_url)):
+            urls.add(urljoin(index_url, url))
+    return urls
