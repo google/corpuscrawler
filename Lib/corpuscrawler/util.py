581f337a3c040fb4e107761304a38229e6d27d13
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-10-19 18:00:37 +0700

    Update util.py

--- Lib/corpuscrawler/util.py
+++ Lib/corpuscrawler/util.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from __future__ import absolute_import, print_function, unicode_literals
+from builtins import open, bytes
 import base64
 import codecs
 import collections
@@ -58,7 +59,7 @@ except ImportError:
     from urllib import quote
     from htmlentitydefs import name2codepoint
     py3 = False
-    unichr = unichr
+    unichr = chr
 
 
 FetchResult = collections.namedtuple('FetchResult',
