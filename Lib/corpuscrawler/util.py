491e06039c31117a8116046a7aa6c9a0662ce589
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-10-19 18:01:58 +0700

    Update util.py

--- Lib/corpuscrawler/util.py
+++ Lib/corpuscrawler/util.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 from __future__ import absolute_import, print_function, unicode_literals
-from builtins import open, bytes
+from io import open
 import base64
 import codecs
 import collections
